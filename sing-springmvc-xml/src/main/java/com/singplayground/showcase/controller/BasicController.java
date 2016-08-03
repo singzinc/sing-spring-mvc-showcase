173e78c83912a9cec4533f588533f44f8e68b791
sing <sing.zinc.hk@gmail.com>
2016-08-03 17:13:31 +0800

    update the function

--- sing-springmvc-xml/src/main/java/com/singplayground/showcase/controller/BasicController.java
+++ sing-springmvc-xml/src/main/java/com/singplayground/showcase/controller/BasicController.java
@@ -1,8 +1,11 @@
 package com.singplayground.showcase.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,13 +13,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class BasicController {
 
 	@RequestMapping(value = "test", method = RequestMethod.GET)
-	public ModelAndView editAgency() throws Exception {
-
-		System.out.println("********");
+	public ModelAndView testController() throws Exception {
+		System.out.println("this is test controller");
 		ModelAndView mav = new ModelAndView();
-
 		mav.setViewName("basic/index");
 		return mav;
+	}
+
+	@RequestMapping(value = "test2", method = RequestMethod.GET)
+	@ResponseBody
+	public HashMap testController2() throws Exception {
+		System.out.println("this is test controller 2");
+		HashMap hm = new HashMap();
+		hm.put("status", "Y");
+		return hm;
 	}
 
 }
